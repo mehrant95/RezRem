31953c7feef8e42abe39d007e9b692731c855ae6
M3hRaN <m3hrantorki@gmail.com>
2016-01-13 02:27:24 +0330

    login page

--- src/RezRem/Main.java
+++ src/RezRem/Main.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
 	
@@ -20,14 +21,16 @@ public class Main extends Application {
        
        pane.getChildren().add(browserView);
        
-       Scene scene = new Scene(pane, 700, 500);
+       Scene scene = new Scene(pane, 380, 500);
+       
+       primaryStage.initStyle(StageStyle.UNDECORATED);
        
        primaryStage.setScene(scene);
        
        primaryStage.show();
        
-       browser.loadURL("http://www.google.com");
-   
+       browser.loadURL(Main.class.getResource("templates/login.html").toExternalForm());
+       
    }
    
    public static void main(String[] args) {
